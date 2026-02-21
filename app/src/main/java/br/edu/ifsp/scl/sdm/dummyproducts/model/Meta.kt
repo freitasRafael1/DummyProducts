7635517aa9f8e79dd1766ede5eb32e8ef32873f2
RafaelCostaFreitas <freitas.rafael1@aluno.ifsp.edu.br>
2026-02-21 12:36:06 -0300

    parte 3

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/model/Meta.kt
@@ -0,0 +1,8 @@
+package br.edu.ifsp.scl.sdm.dummyproducts.model
+
+data class Meta(
+    val barcode: String,
+    val createdAt: String,
+    val qrCode: String,
+    val updatedAt: String
+)
--- app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dummyproducts/ui/MainActivity.kt
@@ -1,14 +1,54 @@
 package br.edu.ifsp.scl.sdm.dummyproducts.ui
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.view.View
+import android.view.ViewParent
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.scl.sdm.dummyproducts.R
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductAdapter
+import br.edu.ifsp.scl.sdm.dummyproducts.adapter.ProductImageAdapter
 import br.edu.ifsp.scl.sdm.dummyproducts.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.sdm.dummyproducts.model.Product
+import br.edu.ifsp.scl.sdm.dummyproducts.model.ProductList
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import java.io.BufferedInputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection.HTTP_OK
+import java.net.URL
+import java.nio.Buffer
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val productList: MutableList<Product> = mutableListOf()
+    private val productAdapter: ProductAdapter by lazy {
+        ProductAdapter(
+            this,
+            productList
+        )
+    }
+    private val productImagesList: MutableList<Bitmap> = mutableListOf()
+    private val productImagesAdapter: ProductImageAdapter by lazy {
+        ProductImageAdapter(this, productImagesList)
+    }
+
+
+    companion object {
+        const val PRODUCTS_ENDPOINT = "https://dummyjson.com/products/"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
@@ -16,5 +56,95 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(amb.mainTb.apply {
             title = getString(R.string.app_name)
         })
+
+        amb.productsSp.apply {
+            adapter = productAdapter
+            onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                //quando o ususario selecionar um produto esse listener vai tratar de buscar a imagem do produto
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val size = productImagesList.size
+                    productImagesList.clear()
+                    productImagesAdapter.notifyItemRangeRemoved(0, size)
+                    retriveProductsImages(productList[position])
+                }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        // NSA
+
+
+
+                }
+            }
+            }
+
+
+
+
+        amb.productImagesRv.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = productImagesAdapter
+        }
+
+        retriveProducts()
     }
+
+
+    private fun retriveProducts() =    Thread {
+            val productsConnection = URL(PRODUCTS_ENDPOINT).openConnection() as HttpsURLConnection
+            try {
+                if (productsConnection.responseCode == HTTP_OK) {
+                    InputStreamReader(productsConnection.inputStream).readText().let {
+                        runOnUiThread {
+                            productAdapter.addAll(Gson().fromJson(it, ProductList::class.java).products)
+                        }
+
+                    }
+                } else {
+                    runOnUiThread {  Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT)
+                        .show()}
+                }
+            } catch (ioe: IOException) {
+                runOnUiThread {  Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT)
+                    .show()}
+            } catch (jse: JsonSyntaxException) {
+                runOnUiThread {  Toast.makeText(this, getString(R.string.response_problem), Toast.LENGTH_SHORT)
+                    .show()}
+            } finally {
+                productsConnection.disconnect()
+            }
+
+        }
+
+    .start()
+
+    private fun retriveProductsImages(product: Product) = Thread{
+        product.images.forEach { imageUrl ->
+            val imageConnection = URL(imageUrl).openConnection() as HttpsURLConnection
+
+            try {
+                if (imageConnection.responseCode == HTTP_OK) {
+                    BufferedInputStream(imageConnection.inputStream).let{
+                        val imageBitmap = BitmapFactory.decodeStream(it)
+                        runOnUiThread {
+                            productImagesList.add(imageBitmap)
+                            productImagesAdapter.notifyItemInserted(productImagesList.lastIndex)
+                        }
+                    }
+                } else {
+                    runOnUiThread {  Toast.makeText(this, getString(R.string.request_problem), Toast.LENGTH_SHORT).show()}
+                }
+            } catch (ioe: IOException) {
+                runOnUiThread {  Toast.makeText(this, getString(R.string.connection_failed), Toast.LENGTH_SHORT).show()}
+            }  finally {
+                imageConnection.disconnect()
+            }
+
+        }
+    }.start()
+
 }
